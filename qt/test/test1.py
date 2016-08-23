3ba8c27c16e33fd81f9a1bc1e887af692584d12c
Aleksandar Nikov <aleksandar.nikov@netcetera.com>
2016-08-23 19:09:40 +0200

    next

--- qt/test/test1.py
+++ qt/test/test1.py
@@ -4,6 +4,35 @@ import glob
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
  
+ 
+class MyWidget(QWidget):
+    def mousePressEvent(self, event):   
+        print self.panel.width()
+        print event.x()
+        print len(self.panel.list_of_files)
+        x = 1.0 * event.x() / self.panel.width() * len(self.panel.list_of_files)
+        print x
+        self.panel.update(int(x))
+    def setPanel(self, panelWidget):
+        self.panel = panelWidget;
+        
+ 
+class PanelWidget(QWidget):
+    firstcall = 1;
+    def __init__(self, list_of_files):
+        QWidget.__init__(self)
+        self.list_of_files = list_of_files
+    def update(self, index):
+        if self.firstcall == 0:
+            self.layout().removeWidget(self.child)
+            self.child.setParent(None)
+        self.firstcall = 0
+        self.child = QLabel()
+        pixmap = QPixmap(self.list_of_files[index])
+        pixmap = pixmap.scaled(400, 400, Qt.KeepAspectRatio)
+        self.child.setPixmap(pixmap)
+        self.layout().addWidget(self.child)
+    
 # Create window
 app = QApplication(sys.argv)
 w = QWidget()
@@ -28,12 +57,12 @@ for fileName in list_of_files1:
         pixmap = pixmap.transformed(QTransform().rotate(180))
         label.setPixmap(pixmap)
     n = n + 1
-    if n == 10:
+    if n == len(list_of_files1) / 10:
         k = k + 1
         n = 0
         
 
-w2 = QWidget()
+w2 = MyWidget()
 l2 = QHBoxLayout(w2)
 ll.addWidget(w2)
 s = "c:\\local\\agro\\testsample\\rgb\\";
@@ -48,9 +77,15 @@ for fileName in list_of_files2:
         pixmap = pixmap.scaled(200, 200, Qt.KeepAspectRatio)
         label.setPixmap(pixmap)
     n = n + 1
-    if n == 20:
+    if n == len(list_of_files2) / 10:
         k = k + 1
         n = 0
+
+w3 = PanelWidget(list_of_files2)
+l3 = QHBoxLayout(w3)
+ll.addWidget(w3)
+w3.update(0)
+w2.setPanel(w3)
 
 # Create widget
 #w.resize(pixmap.width(),pixmap.height())
